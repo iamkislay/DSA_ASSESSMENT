6e020fc0e299eba7c2dffedb41d8bcf9a7891bdf
Kislay Raj <106864096+iamkislay@users.noreply.github.com>
2023-06-15 18:48:17 +0530

    Add files via upload

--- DSA_TEST_1/src/perfectNumberInRange.java
+++ DSA_TEST_1/src/perfectNumberInRange.java
@@ -4,29 +4,29 @@ public class perfectNumberInRange {
 	 public static ArrayList<Integer> perfect(int n) {
 	     if(n==1){
 	         ArrayList<Integer> result=new ArrayList<Integer>();
-	         result.add(1);
+	         //result.add(null);
 	         return result;
 	     }
 	     ArrayList<Integer> result=new ArrayList<Integer>();
 	     result =  perfect(n-1);
 	     
-	     if(n==isPerfect(n,1,0)){
+	     if(n==(int)isPerfect(n,1)){
 	         result.add(n);
 	     }
 	     return result;
 	 }
 
 	    
-	 public static long isPerfect(int n,int i,long sum) {
+	 public static long isPerfect(int n,int i) {
 	        // your solutions goes here..
-		    
-	        if(i>n/2) {
-	        	return 0;
-	        }
-	        if(n%i==0)
-	        	sum=isPerfect(n,i+1,sum)+i;
-	        
-	        return sum;
+		 if(i==n/2+1) {
+			 return 0;
+		 }
+		 long sum=isPerfect(n,i+1);
+		 if(n%i==0) {
+			 sum=sum+i;
+		 }
+		 return sum;
 	    }
 	 public static void main(String[] args) throws IOException {
 	        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
